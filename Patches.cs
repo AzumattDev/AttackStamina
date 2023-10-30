0c1bfd14971cc0c56a0a2937be192581b7d24fdf
AzumattDev <azumattgaming@gmail.com>
2023-10-30 02:37:09 -0400

    Initial commit

---
+++ Patches.cs
@@ -0,0 +1,213 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AttackStamina;
+
+[HarmonyPatch(typeof(Hud), nameof(Hud.Awake))]
+static class HudAwakePatch
+{
+    static void Postfix(Hud __instance)
+    {
+        AttackStaminaPlugin.flag1 = true;
+        AttackStaminaPlugin.AttackStaminaLogger.LogDebug("Instantiating AttackStamina Bar");
+        HudFixedUpdatePatch._StaminaBar = UnityEngine.Object.Instantiate(AttackStaminaPlugin.StaminaUI, __instance.transform).GetComponentInChildren<Slider>();
+        HudFixedUpdatePatch._StaminaBar.gameObject.SetActive(false);
+        HudFixedUpdatePatch._StaminaBar.GetComponent<RectTransform>().transform.Rotate(new Vector3(0.0f, 0.0f, -90f));
+    }
+}
+
+[HarmonyPatch(typeof(Character), nameof(Character.Jump))]
+static class CharacterJumpPatch
+{
+    static void Prefix(Character __instance)
+    {
+        if (AttackStaminaPlugin.useAttackWhenDrained.Value == AttackStaminaPlugin.Toggle.Off || __instance.HaveStamina(__instance.m_jumpStaminaUsage))
+            return;
+        if (AttackStaminaPlugin.attackStamina - (double)__instance.m_jumpStaminaUsage >= 0.0 && __instance.IsOnGround())
+        {
+            __instance.m_jumpForceTiredFactor = 1f;
+            HumanoidStartAttackPatch.useAttackStaminaMod(__instance.m_jumpStaminaUsage);
+        }
+        else if (AttackStaminaPlugin.attackStamina - (double)__instance.m_jumpStaminaUsage < __instance.m_jumpStaminaUsage)
+        {
+            __instance.m_jumpForceTiredFactor = AttackStaminaPlugin.noStaminaJumpForce.Value;
+            Hud.instance.StaminaBarEmptyFlash();
+        }
+        else
+            Hud.instance.StaminaBarEmptyFlash();
+    }
+}
+
+[HarmonyPatch(typeof(Player), nameof(Player.CheckRun))]
+static class PlayerCheckRunPatch
+{
+    static void Prefix(Player __instance) => AttackStaminaPlugin.UseAttackInstead(__instance);
+}
+
+[HarmonyPatch(typeof(Character), nameof(Character.OnSwimming))]
+static class CharacterOnSwimmingPatch
+{
+    static void Prefix(Character __instance)
+    {
+        if (AttackStaminaPlugin.useAttackWhenDrained.Value == AttackStaminaPlugin.Toggle.Off || __instance.HaveStamina())
+            return;
+        if (AttackStaminaPlugin.attackStamina > 0.0)
+        {
+            __instance.AddStamina(1f);
+            HumanoidStartAttackPatch.useAttackStaminaMod(AttackStaminaPlugin.noStaminaSprintDrain.Value);
+        }
+        else
+            Hud.instance.StaminaBarEmptyFlash();
+    }
+}
+
+[HarmonyPatch(typeof(Player), nameof(Player.UpdateCrouch))]
+static class PlayerUpdateCrouchPatch
+{
+    static void Prefix(Player __instance) => AttackStaminaPlugin.UseAttackInstead(__instance, true);
+}
+
+[HarmonyPatch(typeof(Humanoid), nameof(Humanoid.StartAttack))]
+static class HumanoidStartAttackPatch
+{
+    internal static Player player1;
+    internal static bool DoAttack = true;
+
+    static bool Prefix(Humanoid __instance)
+    {
+        float attackStamina = __instance.GetCurrentWeapon().m_shared.m_attack.m_attackStamina;
+        if (!__instance.IsPlayer() || AttackStaminaPlugin.attackStamina - (double)attackStamina >= 0.0 && DoAttack)
+            return true;
+        player1 = (Player)__instance;
+        if (AttackStaminaPlugin.attackStamina >= (double)attackStamina)
+        {
+            DoAttack = true;
+            return true;
+        }
+
+        if (player1.GetStamina() - (double)attackStamina >= 0.0 && AttackStaminaPlugin.attackStamina - (double)attackStamina < 0.0)
+        {
+            if (AttackStaminaPlugin.useNormWhenDrained.Value == AttackStaminaPlugin.Toggle.Off)
+                return false;
+            AttackStaminaPlugin.attackStamina = 0.0f;
+            return true;
+        }
+
+        Hud.instance.StaminaBarEmptyFlash();
+        DoAttack = false;
+        return false;
+    }
+
+    public static void useAttackStaminaMod(float v)
+    {
+        AttackStaminaPlugin.attackStamina -= v;
+        AttackStaminaPlugin.counter = 0;
+        AttackStaminaPlugin.displayCounter = 0;
+        AttackStaminaPlugin.flag = true;
+        AttackStaminaPlugin.hasUsedRecently = true;
+    }
+}
+
+[HarmonyPatch(typeof(Attack), nameof(Attack.GetAttackStamina))]
+static class AttackGetAttackStaminaPatch
+{
+    static void Prefix(Attack __instance)
+    {
+        if (__instance.m_character.IsPlayer())
+        {
+            if ((double)AttackStaminaPlugin.attackStamina - (double)__instance.m_attackStamina >= 0.0 && __instance.m_character.IsPlayer())
+            {
+                HumanoidStartAttackPatch.DoAttack = true;
+                HumanoidStartAttackPatch.useAttackStaminaMod(__instance.m_attackStamina);
+            }
+            else if ((double)AttackStaminaPlugin.attackStamina <= 0.0 && (double)((Player)__instance.m_character).GetStamina() > 0.0 && (double)((Player)__instance.m_character).GetStamina() - (double)__instance.m_attackStamina >= 0.0)
+            {
+                HumanoidStartAttackPatch.DoAttack = true;
+                ((Player)__instance.m_character).UseStamina(__instance.m_attackStamina);
+            }
+
+            __instance.m_attackStamina = 0.0f;
+        }
+    }
+}
+
+[HarmonyPatch(typeof(Player), nameof(Player.UpdateAttackBowDraw))]
+static class PlayerUpdateAttackBowDrawPatch
+{
+    static void Prefix(Player __instance)
+    {
+        ItemDrop.ItemData currentWeapon = __instance.GetCurrentWeapon();
+        if (!__instance.IsDrawingBow())
+            return;
+        float num = AttackStaminaPlugin.attackStamina > 0.0 ? 1f : 0.0f;
+        float drawStaminaDrain = currentWeapon.GetDrawStaminaDrain();
+        if (__instance.GetAttackDrawPercentage() >= 1.0)
+            drawStaminaDrain *= 0.5f;
+        HumanoidStartAttackPatch.useAttackStaminaMod(drawStaminaDrain * Time.fixedDeltaTime);
+        __instance.UseStamina(-num * drawStaminaDrain * Time.fixedDeltaTime);
+    }
+}
+
+[HarmonyPatch(typeof(Hud), nameof(Hud.OnDestroy))]
+static class HudOnDestroyPatch
+{
+    static void Prefix(Hud __instance) => AttackStaminaPlugin.flag1 = false;
+}
+
+[HarmonyPatch(typeof(Hud), nameof(Hud.FixedUpdate))]
+static class HudFixedUpdatePatch
+{
+    internal static Slider _StaminaBar;
+
+    static void Prefix(Hud __instance)
+    {
+        if (AttackStaminaPlugin.flag)
+            ++AttackStaminaPlugin.counter;
+        if (AttackStaminaPlugin.hasUsedRecently && AttackStaminaPlugin.attackStamina >= (double)AttackStaminaPlugin.MaxAttackStamina.Value)
+            ++AttackStaminaPlugin.displayCounter;
+        if (AttackStaminaPlugin.attackStamina <= (double)AttackStaminaPlugin.MaxAttackStamina.Value && IsRegening(AttackStaminaPlugin.counter))
+            AttackStaminaPlugin.attackStamina += 0.5f * AttackStaminaPlugin.AttackStaminaRecharge.Value;
+        if (AttackStaminaPlugin.attackStamina > (double)AttackStaminaPlugin.MaxAttackStamina.Value)
+        {
+            AttackStaminaPlugin.attackStamina = AttackStaminaPlugin.MaxAttackStamina.Value;
+            AttackStaminaPlugin.flag = false;
+        }
+
+        if (!AttackStaminaPlugin.flag1)
+            return;
+        Slider componentInChildren = _StaminaBar.GetComponentInChildren<Slider>();
+        componentInChildren.maxValue = AttackStaminaPlugin.MaxAttackStamina.Value;
+        componentInChildren.normalizedValue = AttackStaminaPlugin.attackStamina / 100f;
+        componentInChildren.value = AttackStaminaPlugin.attackStamina;
+        _StaminaBar.gameObject.SetActive(isShowing());
+        //AttackStaminaPlugin.AttackStaminaLogger.LogError("Is Showing: " + isShowing());
+        Transform? parent = _StaminaBar.transform.parent.parent;
+        RectTransform component1 = parent.GetComponent<RectTransform>();
+        RectTransform component2 = parent.parent.GetChild(0).GetChild(2).GetComponent<RectTransform>();
+        component1.anchorMin = new Vector2(0.5f, 0.0f);
+        component1.anchorMax = new Vector2(0.5f, 0.0f);
+        _StaminaBar.GetComponent<RectTransform>().sizeDelta = new Vector2(component2.sizeDelta.x - 12f, component2.sizeDelta.y - 15f);
+        component1.anchoredPosition = new Vector2(0.0f, component2.anchoredPosition.y + 30f);
+    }
+
+    public static bool IsRegening(int counter)
+    {
+        if (counter <= AttackStaminaPlugin.timeTillCharging.Value)
+            return false;
+        counter = 0;
+        AttackStaminaPlugin.flag = false;
+        return true;
+    }
+
+    public static bool isShowing()
+    {
+        if (!AttackStaminaPlugin.hasUsedRecently)
+            return false;
+        if (AttackStaminaPlugin.displayCounter <= AttackStaminaPlugin.displayTime.Value)
+            return true;
+        AttackStaminaPlugin.hasUsedRecently = false;
+        AttackStaminaPlugin.displayCounter = 0;
+        return false;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using JetBrains.Annotations;
+using ServerSync;
+using UnityEngine;
+
+namespace AttackStamina
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class AttackStaminaPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "AttackStamina";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        internal static string ConnectionError = "";
+        private readonly Harmony _harmony = new(ModGUID);
+        public static readonly ManualLogSource AttackStaminaLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+        private static readonly ConfigSync ConfigSync = new(ModGUID) { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+        public static bool flag = true;
+        public static bool flag1 = false;
+        public static bool hasUsedRecently = false;
+        public static bool bowDrawn = false;
+        public static float attackStamina = 100f;
+        public static int counter = 100;
+        public static int displayCounter = 0;
+        public static GameObject StaminaUI;
+
+        public enum Toggle
+        {
+            On = 1,
+            Off = 0
+        }
+
+        public void Awake()
+        {
+            _serverConfigLocked = config("1 - General", "Lock Configuration", Toggle.On, "If on, the configuration is locked and can be changed by server admins only.");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+            displayTime = config("2 - UI", "Time Until Full Bar disappears", 75, "Time until full bar disappears", false);
+            timeTillCharging = config("3 - Recharge", "Time Until the Bar Starts to Recharge", 75, "Time until bar starts to recharge");
+            AttackStaminaRecharge = config("4 - Values", "Attack Stamina Recharge Rate", 1f, "Multiple of how fast the stamina bar recharges");
+            MaxAttackStamina = config("4 - Values", "Maximum Attack Stamina", 100f, "Maximum Attack Stamina");
+            noStaminaJumpForce = config("4 - Values", "Jump Height When Attack Stamina is Out", 0.6f, "Jump height when out of stamina");
+            noStaminaSprintDrain = config("4 - Values", "Attack Stamina drain when normal is Out", 2f, "Attack Stamina drain when normal is out");
+            useNormWhenDrained = config("5 - Game Rules", "Use Normal Stamina When Attack Stamina is Out", Toggle.On, "Use normal stamina when attack stamina is out");
+            useAttackWhenDrained = config("5 - Game Rules", "Use Attack stamina when Normal Stamina is Out", Toggle.On, "Use Attack stamina when normal stamina is out");
+
+            LoadAssets();
+
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private static AssetBundle GetAssetBundleFromResources(string filename)
+        {
+            Assembly execAssembly = Assembly.GetExecutingAssembly();
+            string resourceName = execAssembly.GetManifestResourceNames()
+                .Single(str => str.EndsWith(filename));
+
+            using (Stream? stream = execAssembly.GetManifestResourceStream(resourceName))
+            {
+                return AssetBundle.LoadFromStream(stream);
+            }
+        }
+
+        public static void LoadAssets()
+        {
+            AssetBundle? assetBundle = GetAssetBundleFromResources("attackstaminabar");
+            StaminaUI = assetBundle.LoadAsset<GameObject>("AttackStaminaBar");
+            assetBundle?.Unload(false);
+        }
+
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                AttackStaminaLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                AttackStaminaLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                AttackStaminaLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+        public static bool UseAttackInstead(Player currentPlayer, bool crouch = false)
+        {
+            if (useAttackWhenDrained.Value == Toggle.Off || currentPlayer.HaveStamina())
+                return false;
+            if (attackStamina > 0.0 && !currentPlayer.IsSneaking() | crouch)
+            {
+                currentPlayer.AddStamina(1f);
+                HumanoidStartAttackPatch.useAttackStaminaMod(noStaminaSprintDrain.Value);
+                return true;
+            }
+
+            Hud.instance.StaminaBarEmptyFlash();
+            return true;
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<Toggle> _serverConfigLocked = null!;
+        public static ConfigEntry<int> displayTime;
+        public static ConfigEntry<int> timeTillCharging;
+        public static ConfigEntry<float> AttackStaminaRecharge;
+        public static ConfigEntry<float> MaxAttackStamina;
+        public static ConfigEntry<float> noStaminaJumpForce;
+        public static ConfigEntry<float> noStaminaSprintDrain;
+        public static ConfigEntry<Toggle> useNormWhenDrained;
+        public static ConfigEntry<Toggle> useAttackWhenDrained;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            [UsedImplicitly] public int? Order = null!;
+            [UsedImplicitly] public bool? Browsable = null!;
+            [UsedImplicitly] public string? Category = null!;
+            [UsedImplicitly] public Action<ConfigEntryBase>? CustomDrawer = null!;
+        }
+
+        class AcceptableShortcuts : AcceptableValueBase
+        {
+            public AcceptableShortcuts() : base(typeof(KeyboardShortcut))
+            {
+            }
+
+            public override object Clamp(object value) => value;
+            public override bool IsValid(object value) => true;
+
+            public override string ToDescriptionString() =>
+                "# Acceptable values: " + string.Join(", ", UnityInput.Current.SupportedKeyCodes);
+        }
+
+        #endregion
+    }
+
+    public static class KeyboardExtensions
+    {
+        public static bool IsKeyDown(this KeyboardShortcut shortcut)
+        {
+            return shortcut.MainKey != KeyCode.None && Input.GetKeyDown(shortcut.MainKey) && shortcut.Modifiers.All(Input.GetKey);
+        }
+
+        public static bool IsKeyHeld(this KeyboardShortcut shortcut)
+        {
+            return shortcut.MainKey != KeyCode.None && Input.GetKey(shortcut.MainKey) && shortcut.Modifiers.All(Input.GetKey);
+        }
+    }
+}
